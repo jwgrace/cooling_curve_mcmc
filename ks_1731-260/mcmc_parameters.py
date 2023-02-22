2169a9a17e6b50f7fd505981308bcb14cce060e0
Justin Grace <graceju1@msu.edu>
2023-02-22 09:48:35 -0500

    First commit

---
+++ ks_1731-260/mcmc_parameters.py
@@ -0,0 +1,132 @@
+import numpy as np
+import subprocess
+from astropy import constants as const
+
+# Directory for storing inlist, LOGS, and MCMC output files.
+model_directory = 'log_epochs'
+inlist = 'inlist'
+LOGS = '{}/LOGS'.format(model_directory)
+
+source_name = 'KS 1731-260'
+
+corner_plot_title = source_name
+corner_plot_file = '{}/corner-ks_1731-260.png'.format(model_directory)
+
+best_fit_plot_title = source_name
+best_fit_plot_file = '{}/cooling_curve-ks_1731-260.png'.format(model_directory)
+
+autocorrelations_plot_title = 'Autocorrelations - {}'.format(source_name)
+autocorrelations_plot_file = '{}/autocorrelations-ks_1731-260.png'.format(model_directory)
+
+mcmc_output_file = '{}/output-ks_1731-260'.format(model_directory)
+
+# dStar will output Teff for every epoch (accretion and observation) excluding the starting time of the first epoch.
+# For calculating a Chi^2 for the cooling curve we only want the observation epochs.
+# This array will mask which of the Teff outputs we want to use for calculating the Chi^2.
+is_observation_epoch = np.array([False, False, False, False, False, False, True, True, True, True, True, True, True, True])
+# Times of observations of the cooling curve in days after end of outburst.
+observation_times = np.array([65.1, 235.7, 751.6, 929.5, 1500.5, 1582.9, 3039.7, 5312.1])
+# Effective temperature in eV measured at the observation times.
+Teff_obs = np.array([104.6, 89.5, 76.4, 73.8, 71.7, 70.3, 64.5, 64.4])
+# Uncertainties of the temperature observations in eV.
+Teff_obs_sigma = np.array([1.3, 1.03, 1.8, 1.9, 1.4, 1.9, 1.8, 1.2])
+# Boltzmann constant for converting temperature from eV to MK
+k_B = const.k_B.to('eV/K').value
+# Observed effective temperature in MK. Initial data is in eV and is converted.
+Teff_obs *= 10**-6/k_B
+# Error on effective temperature in MK. Initial data is in eV and is converted.
+Teff_obs_sigma *= 10**-6/k_B
+
+# Maximum number of MCMC walker steps and number of walkers.
+max_n = 500
+nwalkers = 32
+# The MCMC can be considered converged if the number of steps is autocorrelation_convergence_factor times the maximum autocorrelation time.
+autocorrelation_convergence_factor = 100
+samples_file = '{}/samples-ks_1731-260.h5'.format(model_directory)
+parameter_distributions_array = '{}/parameter_distributions.npy'.format(model_directory)
+autocorrelations_array = '{}/autocorrelations.npy'.format(model_directory)
+
+# List of names of parameters to vary as they appear in the dStar inlist.
+# Must also have an option as a command line argument (set in run.f file).
+# Options include: ['core_mass', 'core_radius', 'core_temperature', 'Qimp', 'Q_heating_shallow', 'Q_heating_inner']
+parameter_names = np.array(['core_temperature', 'Qimp', 'Q_heating_shallow', 'Q_heating_inner'])
+# Command line options corresponding to each parameter.
+# Options include: ['-M', '-R', '-T', '-Q', '-s', '-i']
+parameter_options = np.array(['-T', '-Q', '-s', '-i'])
+
+# Initial guess for parameter values.
+parameter_values_initial = np.array([1.0e8, 4.0, 2.0, 1.5])
+
+# Minumum and maximum values for each parameter.
+# We use these values to set uniform priors bounded by a lower and upper limit.
+# To have no prior, set the minimum values to -np.inf and the maximum values to np.inf
+parameter_minimum_values = np.array([1.0e7, 0.0, 0.0, 0.0])
+parameter_maximum_values = np.array([1.0e9, 40.0, 40.0, 40.0])
+
+# The number of observations to compare the model to.
+num_obs = Teff_obs.shape[0]
+
+def log_prior_uniform(parameter_values, parameter_minimum_values, parameter_maximum_values):
+    if np.any(parameter_values < parameter_minimum_values) or np.any(parameter_values > parameter_maximum_values):
+        return -np.inf
+    else:
+        volume = np.prod(parameter_maximum_values - parameter_minimum_values)
+        return np.log(1/volume)
+
+def calculate_chi2(parameter_values):
+    '''
+    Calculates the reduced Chi^2 of a dStar model with respect to observed effective temperature (Teff_obs).
+
+    Only takes the parameter_values array as an argument for convenient use with emcee. The number of observations (num_obs), inlist, LOGS directory, parameter options, 
+    Teff_obs, and Teff_obs_sigma must all be assigned outside the function.
+
+    Parameters
+    ----------
+
+    parameter_values: array-like'
+        Input array of parameters for dStar model.
+        Must modify the src/run.f file to accomodate command line arguments for each parameter.
+        Parameters here are: [core_temperature, Qimp, Q_heating_shallow]
+
+    Returns
+    -------
+
+    chi2: float
+        The reduced Chi^2 of the dStar model Teff.
+
+    '''
+    # Run the models and calculate the log of the probability.
+    
+    # The number of degrees of freedom of the model.
+    dof = num_obs - parameter_values.shape[0]
+
+    # The list of arguments to be used in the command line to run the model
+    # We don't need history files for these runs so set write_interval_for_history to a large value so that it never writes.
+    arguments = ['./run_dStar', '-I', inlist, '-H', '1000']+[str(option) for parameter in zip(parameter_options, parameter_values) for option in parameter]
+    
+    # Try to run the dStar model which calculates and outputs the Teff.
+    # If the model can't run or returns some sort of error, we will assume that this set of parameters is invalid and return np.inf.
+    try:
+        Teff = np.array(subprocess.run(arguments, capture_output=True, text=True).stdout.split(), dtype=float)[is_observation_epoch]
+        
+        chi2 = np.sum(((Teff - Teff_obs)/Teff_obs_sigma)**2)/dof
+        
+        return chi2
+    except:
+        return np.inf
+
+def log_probability(parameter_values):
+    '''
+    Calculates the log of the probability of a dStar model for an MCMC.
+    
+    The log of the probability is based on the chi^2 error assuming a Gaussian probability distribution: probability = exp(-chi2/2). I ignore leading coefficients since 
+    the MCMC only uses ratios of probabilities so they would cancel anyway.
+
+    Uses uniform priors for all model parameters with given minimum values and maximum values.
+    
+    Only takes the parameter_values array as an argument for convenient use with emcee.
+    '''
+    
+    chi2 = calculate_chi2(parameter_values)
+
+    return log_prior_uniform(parameter_values, parameter_minimum_values, parameter_maximum_values) - chi2/2
---
+++ ks_1731-260/run_mcmc.py
@@ -0,0 +1,110 @@
+import numpy as np
+import importlib
+import sys
+import getopt
+import subprocess
+import emcee
+from multiprocessing import Pool
+import tracemalloc
+import time
+
+tracemalloc.start()
+
+argument_list = sys.argv[1:]
+options = 'P'
+long_options = ['parameters=']
+
+mcmc_parameters_file = 'mcmc_parameters'
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argument_list, options, long_options)
+
+    for argument, value in arguments:
+        if argument in ('-P', '--parameters'):
+            mcmc_parameters_file = value
+
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+    
+# Import file containing all the mcmc variables.
+mcmc = importlib.import_module(mcmc_parameters_file)
+
+# The number of dimensions in parameter space.
+ndim = mcmc.parameter_values_initial.shape[0]
+
+# Array to track autocorrelation times throughout the MCMC.
+autocorrelations = np.full((mcmc.max_n//mcmc.autocorrelation_convergence_factor, ndim), np.nan)
+
+# 2D array of initial positions of the walkers.
+# Center the walkers on the initial guess and randomly vary each parameter by up to 10%.
+pos = mcmc.parameter_values_initial + .1*mcmc.parameter_values_initial*np.random.randn(mcmc.nwalkers, ndim)
+
+backend = emcee.backends.HDFBackend(mcmc.samples_file)
+backend.reset(mcmc.nwalkers, ndim)
+
+i = 0
+
+start = time.time()
+
+with Pool() as pool:
+    sampler = emcee.EnsembleSampler(mcmc.nwalkers, ndim, mcmc.log_probability, pool=pool, backend=backend)
+    #sampler.run_mcmc(pos, max_steps, progress=True)
+
+    # Will use this for testing convergence.
+    old_tau = np.inf
+
+    # Now we'll sample for up to max_n steps
+    for sample in sampler.sample(pos, iterations=mcmc.max_n, progress=True):
+        # Only check convergence every 100 steps
+        if sampler.iteration % mcmc.autocorrelation_convergence_factor:
+            continue
+
+        # Compute the autocorrelation time so far
+        # Using tol=0 means that we'll always get an estimate even if it isn't trustworthy
+        tau = sampler.get_autocorr_time(tol=0)
+        
+        # Save the autocorrelations for this iteration.
+        autocorrelations[i] = tau
+        np.save(mcmc.autocorrelations_array, autocorrelations)
+        i += 1
+
+        # Convergence conditions:
+        # 1. The number of MCMC iterations has exceeded the autocorrelation convergence factor times the greatest autocorrelation time.
+        # 2. The fractional change in the autocorrelation is less than a tolerance of .01.
+        converged = np.all(tau * mcmc.autocorrelation_convergence_factor < sampler.iteration)
+        converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+        if converged:
+            break
+        old_tau = tau
+        
+end = time.time()
+
+# Convert the runtime from seconds to hours, minutes, seconds for easier readability.
+runtime = end - start
+runtime_hours = int(runtime//3600)
+runtime_minutes = int(runtime//60 - 60*runtime_hours)
+runtime_seconds = int(runtime%60)
+
+try:
+    autocorrelations[i] = tau
+except:
+    pass
+
+np.save(mcmc.autocorrelations_array, autocorrelations)
+
+current_memory, peak_memory = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+# Save MCMC walker and runtime output to file.
+f = open(mcmc.mcmc_output_file, 'w')
+
+f.write('{}\n\n'.format(mcmc.source_name))
+f.write('Number of walkers:\t{}\n'.format(mcmc.nwalkers))
+f.write('Number of iterations:\t{}\n'.format(sampler.iteration))
+f.write('Runtime:\t{}:{}:{}\n\n'.format(runtime_hours, runtime_minutes, runtime_seconds))
+f.write('Peak memory:\t{}'.format(peak_memory))
+
+f.close()
